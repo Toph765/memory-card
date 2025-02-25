b3a679dfadf3a55f779753d143aa2e68edb96d75
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-02-25 18:15:53 +0800

    fix: refactor component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
-function Card({src}) {
+function Card({ pokeList}) {
     return (
-        <>
-            <button><img src={src} alt="pokemon" /></button>
-        </>
+        <div className="poke">
+            {console.log('card', pokeList)}
+            {pokeList.map(poke => {
+                return (<button key={poke.id}><img src={poke.img} alt={poke.name} /></button>);
+                
+            })}
+        </div>
     )
 }
 
