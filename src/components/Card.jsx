8ca6f1f528c5f4adac4276e393709c11a23a66d6
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-02-27 18:43:05 +0800

    feat: dynamically show caption

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,30 @@
-function Card({ pokeList, click}) {
+import { useState } from "react";
+
+function Card({ pokeList, click }) {
+    const [isHidden, setIsHidden] = useState(true);
+
+    const handleCheckBox = (e) => {
+        const { checked } = e.target;
+        checked ? setIsHidden(() => false) : setIsHidden(() => true);
+    } 
+
     return (
-        <div className="poke">
-            {pokeList.map(poke => {
-                return (
-                    <button key={poke.id} name={poke.name} onClick={() => click(poke.name)}>
-                        <figure>
-                            <img src={poke.img} alt={poke.name} />
-                            <figcaption>{poke.name}</figcaption>
-                        </figure>
-                </button>);
-                
-            })}
+        <div>
+            <div>
+                <input type="checkbox" onChange={handleCheckBox} id="caption" />
+                <label htmlFor="caption">Show pokemon names</label>
+            </div>
+            <div className="poke">
+                {pokeList.map(poke => {
+                    return (
+                        <button key={poke.id} name={poke.name} onClick={() => click(poke.name)}>
+                            <figure>
+                                <img src={poke.img} alt={poke.name} />
+                                <figcaption hidden={isHidden}>{poke.name}</figcaption>
+                            </figure>
+                    </button>);
+                })}
+            </div>
         </div>
     )
 }
