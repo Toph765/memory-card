2866e4a92332c20aebe58f1c887df33cd24a4ecb
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-02-28 17:32:09 +0800

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import './App.css';
+import './styles/App.css';
 import Board from './components/Board'; 
 import ScoreBoard from './components/ScoreBoard';
 
